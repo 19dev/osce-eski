b48eb47a06b39d2d179ef786deaa63a2dec1ce4c
aşkın gedik <askn@bil.omu.edu.tr>
2012-03-29 16:36:14 +0300

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Q::Application.config.secret_token = 'cd4f8387ebeb00d01ee8cc90220b1c4223feb65dcc949612e1360ed4705054f59920c451b0bf0255b0fa62c894017b608f0b3375c736b26b93da3a21f7bd898b'
