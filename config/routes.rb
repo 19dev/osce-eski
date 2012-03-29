164f6597e0e90a2bce35d8fdf77dd79397793294
aşkın gedik <askn@bil.omu.edu.tr>
2012-03-29 17:15:49 +0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Q::Application.routes.draw do
+  get "home/index"
+
+  root :to => 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,6 @@ Q::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
