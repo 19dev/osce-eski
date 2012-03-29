883d90bde49b235275f0f98b1004dc05e6d69eda
aşkın gedik <askn@bil.omu.edu.tr>
2012-03-29 18:11:33 +0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Q::Application.routes.draw do
   get "home/index"
   root :to => 'home#index'
+  match 'home/index' => 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
